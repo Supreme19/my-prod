9effecf604d5396529e4ab77c7b8cc72944f794f
Volodymyr Hovorun <volodymyr.hovorun@rogmax.com>
2019-06-27 12:59:58 +0300

    Started

---
+++ src/prod/Innn.java
@@ -0,0 +1,11 @@
+package prod;
+
+public class Innn {
+    public static void main(String[] args) {
+        System.out.println("the way started");
+        Sercer item = new Sercer();
+        System.out.println(item.math(10,15));
+    }
+
+}
+
---
+++ src/prod/Sercer.java
@@ -0,0 +1,7 @@
+package prod;
+
+public class Sercer {
+    int math(int a, int b) {
+        return a+b;
+    }
+}
